dbcd0de6ad15ef45cc424dbe5ba1761bb0381bf1
VoAnhBen <benva.ce190709@gmail.com>
2025-08-29 11:27:31 +0700

    67

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,27 +1,38 @@
 import { Button, Input } from "antd";
 import '../style.css'
+import { useState } from "react";
 const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const handleClickBtn = () => {
+        alert("Hi");
+    }
     return (
         <div className="user-form">
             <div>
                 <div>
                     <span>Full Name</span>
-                    <Input style={{ width: "80%" }} />
+                    <Input style={{ width: "80%" }}
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input style={{ width: "80%" }} />
+                    <Input style={{ width: "80%" }} value={email} onChange={(event) => { setEmail(event.target.value) }} />
                 </div>
                 <div className="user-form-password">
                     <span>Password</span>
-                    <Input.Password style={{ width: "80%" }} />
+                    <Input.Password style={{ width: "80%" }} value={password} onChange={(event) => { setPassword(event.target.value) }} />
                 </div>
                 <div>
                     <span>Phone</span>
-                    <Input style={{ width: "80%" }} />
+                    <Input style={{ width: "80%" }} value={phone} onChange={(event) => { setPhone(event.target.value) }} />
                 </div>
                 <div>
-                    <Button style={{ width: "7%" }} type="primary">Create User</Button>
+                    <Button onClick={handleClickBtn} style={{ width: "7%" }} type="primary">Create User</Button>
                 </div>
             </div>
         </div>
